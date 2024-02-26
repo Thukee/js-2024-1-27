287f84b43c5d01e2b57ad541b34cce0e1534282a
Thushara Abeysinghe <thukee@gmail.com>
2024-02-27 00:50:52 +0200

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert("Hello World!");
+let i = 0;
+let a = 0;
+let naturalNumbers = [];
+while (i < 1000) {
+  if (i % 3 == 0) {
+    naturalNumbers[a] = i;
+    a++;
+  } else if (i % 5 == 0) {
+    naturalNumbers[a] = i;
+    a++;
+  }
+  i++;
+}
+console.log(naturalNumbers);
+
+let b = 0;
+let total = 0;
+
+while (b < a) {
+  total = total + naturalNumbers[b];
+  b++;
+}
+console.log(total);
